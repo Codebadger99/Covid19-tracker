fb4c42a60f95d9434bb325d767ccfb2ed5bc3e8c
codebadger99 <ebukanuel2021@gmail.com>
2024-01-16 10:22:51 +0100

    Updated

---
+++ Covid-Tracker/component/spinner/spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './spinner.css'
+
+const spinner = () => {
+  return (
+    <div className='push'>
+        <span className="loader"></span>
+    </div>
+  )
+}
+
+export default spinner
--- Covid-Tracker/src/App.jsx
+++ Covid-Tracker/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { MdCoronavirus } from "react-icons/md";
+import Spinner from "../component/spinner/spinner";
 
 function App() {
   useEffect(() => {
@@ -10,7 +11,8 @@ function App() {
   const [Countries, setCountries] = useState([]);
   const [CovidData, setCovidData] = useState([]);
   const [Country, setCountry] = useState("");
-  const [CountryImg, setImg] = useState([])
+  const [CountryImg, setImg] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   const onChange = (e) => {
     setCountry(e.target.value);
@@ -35,14 +37,18 @@ function App() {
   };
 
   const GetData = () => {
-    fetch("https://disease.sh/v3/covid-19/countries/" + Country)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-        console.log(data.countryInfo)
-        setCovidData(data);
-        setImg(data.countryInfo)
-      });
+    try {
+      setLoading(true);
+      fetch("https://disease.sh/v3/covid-19/countries/" + Country)
+        .then((res) => res.json())
+        .then((data) => {
+          setCovidData(data);
+          setImg(data.countryInfo);
+          setLoading(false);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="App">
@@ -64,32 +70,47 @@ function App() {
           Take Data
         </button>
       </div>
+
       <div>
-        <div className="info">
-          <h1>{Country}</h1>
-        </div>
-      
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="column">
-            <div className="card">
-              <h1>Cases: {CovidData.cases}</h1>
-            </div>
-          </div>
+        {CovidData.length === 0 && CountryImg.length === 0 ? (
+          ""
+        ) : (
+          <div>
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <div>
+                <div className="info">
+                  <h1>{Country}</h1>
+                </div>
+                <div className="img">
+                  <img src={CountryImg.flag} alt="" />
+                </div>
+                <div className="container">
+                  <div className="row">
+                    <div className="column">
+                      <div className="card">
+                        <h1>Cases: {CovidData.cases}</h1>
+                      </div>
+                    </div>
 
-          <div className="column">
-            <div className="card">
-              <h1>Death: {CovidData.deaths}</h1>
-            </div>
-          </div>
+                    <div className="column">
+                      <div className="card">
+                        <h1>Death: {CovidData.deaths}</h1>
+                      </div>
+                    </div>
 
-          <div className="column">
-            <div className="card">
-              <h1>Recovered: {CovidData.recovered}</h1>
-            </div>
+                    <div className="column">
+                      <div className="card">
+                        <h1>Recovered: {CovidData.recovered}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
